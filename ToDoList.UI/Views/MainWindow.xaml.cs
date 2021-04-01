74231bd967b0b6d4d66073f3a8780e80ebdc8c28
Gabriel Dias Gonçalves <gabrieldgon@gmail.com>
2021-04-01 09:36:37 -0300

    Adicionando as camadas iniciais

--- ToDoList.UI/Views/MainWindow.xaml.cs
+++ ToDoList.UI/Views/MainWindow.xaml.cs
@@ -10,13 +10,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ToDoList.UI
+namespace ToDoList.UI.Views
 {
     /// <summary>
-    /// Interação lógica para MainWindow.xam
+    /// Lógica interna para MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
