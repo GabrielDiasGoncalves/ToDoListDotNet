290ce911fff4fb1eb123ce01ca7f1ec500b5ebf3
Gabriel Dias Gonçalves <gabrieldgon@gmail.com>
2021-04-01 14:37:21 -0300

    Adicionando o primeiro repositório

--- ToDoList.Infra/Repositories/TarefaRepository.cs
+++ ToDoList.Infra/Repositories/TarefaRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ToDoList.Infra
+namespace ToDoList.Infra.Repositories
 {
-    public class Class1
+    public class TarefaRepository
     {
     }
 }
