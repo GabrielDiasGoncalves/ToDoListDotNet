9d1cec4847eb5250c4d5c0848af767ffe6c23cc8
Gabriel Dias Gonçalves <gabrieldgon@gmail.com>
2021-04-01 09:55:26 -0300

    Adicionando camada de modelo

---
+++ ToDoList.Domain/Models/Tarefa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Models
+{
+    public class Tarefa
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataTermino { get; set; }
+    }
+}
