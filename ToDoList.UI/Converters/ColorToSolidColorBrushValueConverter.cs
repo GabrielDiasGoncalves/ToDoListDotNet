c6083a34aefaf8dca9f853f3af0f080060836c12
Gabriel Dias Gonçalves <gabrieldgon@gmail.com>
2021-04-12 21:25:16 -0300

    Early return

--- ToDoList.UI/Converters/ColorToSolidColorBrushValueConverter.cs
+++ ToDoList.UI/Converters/ColorToSolidColorBrushValueConverter.cs
@@ -9,13 +9,11 @@ namespace ToDoList.UI.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is System.Drawing.Color)
-            {
-                var color = (System.Drawing.Color)value;
-                return new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
-            }
-
-            return null;
+            if (!(value is System.Drawing.Color)) return null;
+            
+            var color = (System.Drawing.Color)value;
+                
+            return new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
